d579d42e96f2094d08d2b6942959aafb88d4ecb1
Radojcic Kilien Borisz <h376562@stud.u-szeged.hu>
2025-04-11 19:59:54 +0200

    Data verification, logging, temporary shopping page

--- app/src/main/java/com/hu/mobilalk/MainActivity.java
+++ app/src/main/java/com/hu/mobilalk/MainActivity.java
@@ -2,12 +2,20 @@ package com.hu.mobilalk;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +29,47 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         */
+
+        this.email = findViewById(R.id.login_edittext_email);
+        this.password = findViewById(R.id.login_edittext_password);
     }
 
+    // OPENS REGISTER ACTIVITY
     public void btn_register(View view) {
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    // OPENS SHOP ACTIVITY WHEN SUCCESSFUL
     public void btn_login(View view) {
+        String email = this.email.getText().toString();
+        String password_1 = this.password.getText().toString();
 
+        // EMPTY EMAIL
+        if(email.isBlank()) {
+            Toast.makeText(MainActivity.this, "Nincs email megadva!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not input an email address.");
+            return;
+        }
+
+        // WRONG EMAIL FORMAT
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(MainActivity.this, "Helytelen email formátum!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User input a badly formatted email address.");
+            return;
+        }
+
+        // EMPTY PASSWORD
+        if(password_1.isBlank()) {
+            Toast.makeText(MainActivity.this, "Nincs jelszó megadva!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not input password.");
+            return;
+        }
+
+        Toast.makeText(MainActivity.this, "Sikeres bejelentkezés!", Toast.LENGTH_LONG).show();
+        Log.d(LOG_TAG, "User successfully logged in.");
+
+        Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/hu/mobilalk/RegisterActivity.java
+++ app/src/main/java/com/hu/mobilalk/RegisterActivity.java
@@ -2,34 +2,107 @@ package com.hu.mobilalk;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+
+    private EditText email;
+    private EditText password_1;
+    private EditText password_2;
+    private CheckBox data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        */
+
+        this.email = findViewById(R.id.register_edittext_email);
+        this.password_1 = findViewById(R.id.register_edittext_password);
+        this.password_2 = findViewById(R.id.register_edittext_password_verify);
+        this.data = findViewById(R.id.register_checkbox_data);
     }
 
 
+    // OPENS LOGIN ACTIVITY
     public void activity_login(View view) {
         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
+    // OPENS LOGIN ACTIVITY WHEN SUCCESSFUL
     public void register_button(View view) {
+        String email = this.email.getText().toString();
+        String password_1 = this.password_1.getText().toString();
+        String password_2 = this.password_2.getText().toString();
+        boolean data = this.data.isChecked();
 
+        // EMPTY EMAIL
+        if(email.isBlank()) {
+            Toast.makeText(RegisterActivity.this, "Nincs email megadva!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not input an email address.");
+            return;
+        }
+
+        // WRONG EMAIL FORMAT
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(RegisterActivity.this, "Helytelen email formátum!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User input a badly formatted email address.");
+            return;
+        }
+
+        // EMPTY PASSWORD
+        if(password_1.isBlank()) {
+            Toast.makeText(RegisterActivity.this, "Nincs jelszó megadva!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not input password.");
+            return;
+        }
+
+        // EMPTY PASSWORD
+        if(password_2.isBlank()) {
+            Toast.makeText(RegisterActivity.this, "Nincs megerősítő jelszó megadva!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not input the second password.");
+            return;
+        }
+
+        // PASSWORDS DO NOT MATCH
+        if(!password_1.equals(password_2)) {
+            Toast.makeText(RegisterActivity.this, "A jelszók nem egyeznek!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User failed to input the same password twice.");
+            return;
+        }
+
+        // SHORT PASSWORD
+        if(password_1.length() < 8) {
+            Toast.makeText(RegisterActivity.this, "A jelszó túl rövid!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User input a short password.");
+            return;
+        }
+
+        // LONG PASSWORD
+        if(password_1.length() > 32) {
+            Toast.makeText(RegisterActivity.this, "A jelszó túl hosszú!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User input a long password.");
+            return;
+        }
+
+        // DATA USAGE FALSE
+        if(!data) {
+            Toast.makeText(RegisterActivity.this, "El kell fogadnia az adatainak az értékesítését!", Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, "User did not accept the data policy.");
+            return;
+        }
+
+        Toast.makeText(RegisterActivity.this, "Sikeres regisztráció!", Toast.LENGTH_LONG).show();
+        Log.d(LOG_TAG, "User successfully registered!");
     }
 }
