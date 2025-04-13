e25d339369a674a6867c28b636f5b48dba06f540
Radojcic Kilien Borisz <h376562@stud.u-szeged.hu>
2025-04-13 19:25:33 +0200

    Auth, fin

--- app/src/main/java/com/hu/mobilalk/MainActivity.java
+++ app/src/main/java/com/hu/mobilalk/MainActivity.java
@@ -9,7 +9,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
@@ -17,18 +26,16 @@ public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        */
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
 
         this.email = findViewById(R.id.login_edittext_email);
         this.password = findViewById(R.id.login_edittext_password);
@@ -43,33 +50,43 @@ public class MainActivity extends AppCompatActivity {
     // OPENS SHOP ACTIVITY WHEN SUCCESSFUL
     public void btn_login(View view) {
         String email = this.email.getText().toString();
-        String password_1 = this.password.getText().toString();
+        String password = this.password.getText().toString();
 
         // EMPTY EMAIL
         if(email.isBlank()) {
             Toast.makeText(MainActivity.this, "Nincs email megadva!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not input an email address.");
+            Log.d(LOG_TAG, "User did not input an email address.");
             return;
         }
 
         // WRONG EMAIL FORMAT
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(MainActivity.this, "Helytelen email formátum!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User input a badly formatted email address.");
+            Log.d(LOG_TAG, "User input a badly formatted email address.");
             return;
         }
 
         // EMPTY PASSWORD
-        if(password_1.isBlank()) {
+        if(password.isBlank()) {
             Toast.makeText(MainActivity.this, "Nincs jelszó megadva!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not input password.");
+            Log.d(LOG_TAG, "User did not input password.");
             return;
         }
 
-        Toast.makeText(MainActivity.this, "Sikeres bejelentkezés!", Toast.LENGTH_LONG).show();
-        Log.d(LOG_TAG, "User successfully logged in.");
 
-        Intent intent = new Intent(MainActivity.this, ShopActivity.class);
-        startActivity(intent);
+        // CORRECT INFO, ATTEMPT LOGIN
+        Log.d(LOG_TAG, "User is attempting to login with well formatted info...");
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()) {
+                Toast.makeText(MainActivity.this, "Sikeres bejelentkezés!", Toast.LENGTH_LONG).show();
+                Log.d(LOG_TAG, "User logged in!");
+
+                Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Sikertelen bejelentkezés!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/hu/mobilalk/RegisterActivity.java
+++ app/src/main/java/com/hu/mobilalk/RegisterActivity.java
@@ -15,9 +15,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String LOG_TAG = RegisterActivity.class.getName();
@@ -27,11 +33,15 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password_2;
     private CheckBox data;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         View text_decoration = findViewById(R.id.text_decoration);
@@ -57,10 +67,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         animator.setDuration(250);
 
-        animator.addUpdateListener(animation -> {
-            int animatedColor = (int) animation.getAnimatedValue();
-            ((TextView) view).setTextColor(animatedColor);
-        });
+        animator.addUpdateListener(animation -> ((TextView) view).setTextColor((int) animation.getAnimatedValue()));
 
         animator.start();
     }
@@ -81,60 +88,74 @@ public class RegisterActivity extends AppCompatActivity {
         // EMPTY EMAIL
         if(email.isBlank()) {
             Toast.makeText(RegisterActivity.this, "Nincs email megadva!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not input an email address.");
+            Log.d(LOG_TAG, "User did not input an email address.");
             return;
         }
 
         // WRONG EMAIL FORMAT
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(RegisterActivity.this, "Helytelen email formátum!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User input a badly formatted email address.");
+            Log.d(LOG_TAG, "User input a badly formatted email address.");
             return;
         }
 
         // EMPTY PASSWORD
         if(password_1.isBlank()) {
             Toast.makeText(RegisterActivity.this, "Nincs jelszó megadva!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not input password.");
+            Log.d(LOG_TAG, "User did not input password.");
             return;
         }
 
         // EMPTY PASSWORD
         if(password_2.isBlank()) {
             Toast.makeText(RegisterActivity.this, "Nincs megerősítő jelszó megadva!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not input the second password.");
+            Log.d(LOG_TAG, "User did not input the second password.");
             return;
         }
 
         // PASSWORDS DO NOT MATCH
         if(!password_1.equals(password_2)) {
             Toast.makeText(RegisterActivity.this, "A jelszók nem egyeznek!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User failed to input the same password twice.");
+            Log.d(LOG_TAG, "User failed to input the same password twice.");
             return;
         }
 
         // SHORT PASSWORD
         if(password_1.length() < 8) {
             Toast.makeText(RegisterActivity.this, "A jelszó túl rövid!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User input a short password.");
+            Log.d(LOG_TAG, "User input a short password.");
             return;
         }
 
         // LONG PASSWORD
         if(password_1.length() > 32) {
             Toast.makeText(RegisterActivity.this, "A jelszó túl hosszú!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User input a long password.");
+            Log.d(LOG_TAG, "User input a long password.");
             return;
         }
 
         // DATA USAGE FALSE
         if(!data) {
             Toast.makeText(RegisterActivity.this, "El kell fogadnia az adatainak az értékesítését!", Toast.LENGTH_LONG).show();
-            Log.e(LOG_TAG, "User did not accept the data policy.");
+            Log.d(LOG_TAG, "User did not accept the data policy.");
             return;
         }
 
-        Toast.makeText(RegisterActivity.this, "Sikeres regisztráció!", Toast.LENGTH_LONG).show();
-        Log.d(LOG_TAG, "User successfully registered!");
+
+        // CORRECT INFO, ATTEMPT REGISTRATION
+        Log.i(LOG_TAG, "User is attempting to register with well formatted info...");
+        mAuth.createUserWithEmailAndPassword(email, password_1).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()) {
+                Log.i(LOG_TAG, "User created successfully!");
+                Toast.makeText(RegisterActivity.this, "Sikeres regisztráció!", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(RegisterActivity.this, ShopActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Log.e(LOG_TAG, "User creation failed!");
+                Toast.makeText(RegisterActivity.this, "Sikertelen regisztráció!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
